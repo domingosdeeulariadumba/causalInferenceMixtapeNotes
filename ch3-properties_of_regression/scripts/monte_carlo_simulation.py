eec8ba2d62ae0f8fcf4cefe740c58e23c50912fc
Domingos de Eularia Dumba <domingosdeeulariadumba@users.noreply.github.com>
2026-02-04 13:37:50 +0100

    update MC simulation

--- ch3-properties_of_regression/scripts/monte_carlo_simulation.py
+++ ch3-properties_of_regression/scripts/monte_carlo_simulation.py
@@ -1,9 +1,8 @@
+# Dependencies
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
-obs = 10_000
-reps = 1_000
-beta = 2
-beta_estimates = []
+# Observations, repetitions, slope, and an empty slope list 
+obs, reps, beta, beta_estimates = 10_000, 1_000, 2, []
 
 # Monte Carlo Simulation
 for _ in range(reps):
@@ -11,15 +10,17 @@ for _ in range(reps):
     u = np.random.normal(0, 36, obs)
     y = 3 + beta*x + u
     cov_mtx = np.cov(x, y) # np.array([[var(x), cov(x,y)], [cov(y,x), var(y)]])
-    beta_estimates.append(cov_mtx[0, 1] / cov_mtx[0, 0])
-e_beta_expected = round(np.array(beta_estimates).mean())
-assert(e_beta_expected == beta) # E(beta_hat) = beta, unbiasedness holds
+    beta_hat = cov_mtx[0, 1] / cov_mtx[0, 0]
+    beta_estimates.append(beta_hat)
+e_beta_hat = round(np.array(beta_estimates).mean())
+assert(e_beta_hat == beta) # E(beta_hat) = beta, unbiasedness holds
 
 # Summary
-pd.Series(beta_estimates).describe().to_frame('beta').T.iloc[:, :3]
+beta_estimates_series = pd.Series(beta_estimates)
+beta_estimates_series.describe().to_frame('beta').T.iloc[:, :3]
 
 # Distribution
-pd.Series(beta_estimates).plot.hist(color = 'grey', bins = 40, linewidth = .5, edgecolor = 'k' )
+beta_estimates_series.plot.hist(color = 'grey', bins = 40, linewidth = .5, edgecolor = 'k' )
 plt.ylabel('Density')
 plt.xlabel('_b[x]')
 plt.title('Distribution of coefficients from Monte Carlo simulation')
