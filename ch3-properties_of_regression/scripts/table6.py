be5c6bd2b3b1093ceaa186aeccb6d28894febd5d
Domingos de Eularia Dumba <domingosdeeulariadumba@users.noreply.github.com>
2026-02-04 08:05:54 +0100

    Add table 6 script

---
+++ ch3-properties_of_regression/scripts/table6.py
@@ -0,0 +1,34 @@
+# Dependencies
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Data Generation, model fitting, and prediction
+np.random.seed(1234)
+n = 10
+x = 9 * np.random.randn(n)
+u = 36 * np.random.randn(n)
+y = 3 + 2*x + u
+X = x.reshape(-1, 1)
+model = LinearRegression()
+model.fit(X, y)
+yhat = model.predict(X)
+u_hat = y - yhat
+pd.Series(u_hat).describe() # su residuals
+
+# Summarized output
+df = pd.DataFrame({
+    'x': x,
+    'u': u,
+    'y': y,
+    'yhat': yhat,
+    'residuals': u_hat
+})
+print(df)
+
+# collapse (sum) x u y yhat residuals
+collapsed = df.sum().to_frame('Sum').T
+print(collapsed)
+
+# Verifying residuals sum to ~0
+assert(round(u_hat.sum(), 10) == 0)
