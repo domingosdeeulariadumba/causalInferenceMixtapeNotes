679ef1ec6dce4483f7789aa9ce4c97b6cf4c2eae
Domingos de Eularia Dumba <domingosdeeulariadumba@users.noreply.github.com>
2026-02-04 08:02:05 +0100

    Add figures 3 and 4 script and plot

---
+++ ch3-properties_of_regression/scripts/fig3_and_3.py
@@ -0,0 +1,50 @@
+# Dependencies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+# Data Generation, model fitting, and prediction
+np.random.seed(1)
+n = 10_000
+x = np.random.randn(n)
+u = np.random.randn(n)
+y = 5.5 * x + 12 * u
+X = x.reshape(-1, 1)
+model = LinearRegression()
+model.fit(X, y)
+yhat1 = model.predict(X)
+
+intercept, slope = model.intercept_, model.coef_[0]
+yhat2 = intercept + slope * x # just to handle PRNG differences between Stata and Python
+df_yhat = pd.DataFrame({'yhat1': yhat1, 'yhat2': yhat2})
+df_yhat.describe() # sum yhat*
+
+uhat1 = y - yhat1
+uhat2 = y - yhat2
+df_uhat = pd.DataFrame({'uhat1': uhat1, 'uhat2': uhat2})
+df_uhat.describe() # sum uhat*
+
+_, axes = plt.subplots(1, 2, figsize=(14, 5))
+data = pd.DataFrame({'x': x, 'y': y})
+# OLS Regression Line
+sns.regplot(
+    data, x = x , y = y, color = 'k', marker = '.', ax = axes[0],
+    line_kws = dict(linewidth = 1, alpha = .5, label = 'Fitted values y')
+    )
+axes[0].axhline(0, color = 'k', linestyle = '--')
+axes[0].set_xlabel('')
+axes[0].set_ylabel('')
+axes[0].set_title('OLS Regression Line')
+axes[0].legend(loc = 'lower center')
+# Residual vs fitted values plot (rvfplot)
+sns.residplot(
+    data, x = 'x', y = 'y', color = 'grey', ax = axes[1],
+              line_kws = dict(linestyle = '-')
+              )
+axes[1].set_xlabel('Fitted values')
+axes[1].set_ylabel('Residuals')
+axes[1].set_title('Residuals vs Fitted Values')
+plt.tight_layout()
+plt.show()
